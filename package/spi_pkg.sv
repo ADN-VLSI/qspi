54f8dde3b88f6cc8dd0aff19ab677b5f6caf6115
Foez Ahmed <foez.official@gmail.com>
2026-06-15 14:54:20 +0600

    new file:   package/spi_pkg.sv modified:   source/qspi_core.sv

---
+++ package/spi_pkg.sv
@@ -0,0 +1,69 @@
+`ifndef SPI_PKG_SV
+`define SPI_PKG_SV 0
+
+        package spi_pkg;
+
+          localparam int ADDR_CLK_DIV0    = 'h 0000;
+          localparam int ADDR_CLK_DIV1    = 'h 0001;
+          localparam int ADDR_CLK_DIV2    = 'h 0002;
+          localparam int ADDR_CLK_DIV3    = 'h 0003;
+          localparam int ADDR_ADDR0       = 'h 0020;
+          localparam int ADDR_ADDR1       = 'h 0021;
+          localparam int ADDR_ADDR2       = 'h 0022;
+          localparam int ADDR_ADDR3       = 'h 0023;
+          localparam int ADDR_TX_DATA_CNT = 'h 0040;
+          localparam int ADDR_RX_DATA_CNT = 'h 0041;
+          localparam int ADDR_WE_CMD      = 'h 0080;
+          localparam int ADDR_WE_CFG      = 'h 0081;
+          localparam int ADDR_WCMD_CMD    = 'h 0082;
+          localparam int ADDR_WCMD_CFG    = 'h 0083;
+          localparam int ADDR_WADDR_CMD   = 'h 0084;
+          localparam int ADDR_WADDR_CFG   = 'h 0085;
+          localparam int ADDR_WMODE_CMD   = 'h 0086;
+          localparam int ADDR_WMODE_CFG   = 'h 0087;
+          localparam int ADDR_WDATA_CMD   = 'h 0088;
+          localparam int ADDR_WDATA_CFG   = 'h 0089;
+          localparam int ADDR_RCMD_CMD    = 'h 008A;
+          localparam int ADDR_RCMD_CFG    = 'h 008B;
+          localparam int ADDR_RADDR_CMD   = 'h 008C;
+          localparam int ADDR_RADDR_CFG   = 'h 008D;
+          localparam int ADDR_RMODE_CMD   = 'h 008E;
+          localparam int ADDR_RMODE_CFG   = 'h 008F;
+          localparam int ADDR_RDATA_CMD   = 'h 0090;
+          localparam int ADDR_RDATA_CFG   = 'h 0091;
+
+          typedef struct packed {
+                    logic [7:0] CLK_DIV0;
+                    logic [7:0] CLK_DIV1;
+                    logic [7:0] CLK_DIV2;
+                    logic [7:0] CLK_DIV3;
+                    logic [7:0] ADDR0;
+                    logic [7:0] ADDR1;
+                    logic [7:0] ADDR2;
+                    logic [7:0] ADDR3;
+                    logic [7:0] TX_DATA_CNT;
+                    logic [7:0] RX_DATA_CNT;
+                    logic [7:0] WE_CMD;
+                    logic [2:0] WE_CFG;
+                    logic [7:0] WCMD_CMD;
+                    logic [1:0] WCMD_CFG;
+                    logic [2:0] WADDR_CMD;
+                    logic [1:0] WADDR_CFG;
+                    logic [7:0] WMODE_CMD;
+                    logic [2:0] WMODE_CFG;
+                    logic [7:0] WDATA_CMD;
+                    logic [1:0] WDATA_CFG;
+                    logic [7:0] RCMD_CMD;
+                    logic [1:0] RCMD_CFG;
+                    logic [2:0] RADDR_CMD;
+                    logic [1:0] RADDR_CFG;
+                    logic [7:0] RMODE_CMD;
+                    logic [2:0] RMODE_CFG;
+                    logic [7:0] RDATA_CMD;
+                    logic [1:0] RDATA_CFG;
+                  } spi_control_reg_t;
+
+        endpackage
+
+`endif
+
--- source/qspi_core.sv
+++ source/qspi_core.sv
@@ -1,33 +1,6 @@
 module qspi_core (
 
-    input logic [7:0] CLK_DIV0,
-    input logic [7:0] CLK_DIV1,
-    input logic [7:0] CLK_DIV2,
-    input logic [7:0] CLK_DIV3,
-    input logic [7:0] ADDR0,
-    input logic [7:0] ADDR1,
-    input logic [7:0] ADDR2,
-    input logic [7:0] ADDR3,
-    input logic [7:0] TX_DATA_CNT,
-    input logic [7:0] RX_DATA_CNT,
-    input logic [7:0] WE_CMD,
-    input logic [2:0] WE_CFG,
-    input logic [7:0] WCMD_CMD,
-    input logic [1:0] WCMD_CFG,
-    input logic [2:0] WADDR_CMD,
-    input logic [1:0] WADDR_CFG,
-    input logic [7:0] WMODE_CMD,
-    input logic [2:0] WMODE_CFG,
-    input logic [7:0] WDATA_CMD,
-    input logic [1:0] WDATA_CFG,
-    input logic [7:0] RCMD_CMD,
-    input logic [1:0] RCMD_CFG,
-    input logic [2:0] RADDR_CMD,
-    input logic [1:0] RADDR_CFG,
-    input logic [7:0] RMODE_CMD,
-    input logic [2:0] RMODE_CFG,
-    input logic [7:0] RDATA_CMD,
-    input logic [1:0] RDATA_CFG,
+    input spi_control_reg_t control_reg_i,
 
     input logic arst_ni,
     input logic clk_i,
@@ -44,6 +17,6 @@ module qspi_core (
     output logic       data_valid_o,
     input  logic       data_ready_i
 
-);
+  );
 
 endmodule
